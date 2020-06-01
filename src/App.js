ff2ac3e319ec1ecb449d37e82550a01cb4cc275f
gob <gob@GOB-IN.us.oracle.com>
2020-06-01 11:42:58 +0530

    Changed host

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
     let fileType = fileParts[1];
     console.log("Preparing the upload", fileName, fileType);
     
-    fetch("http://ec2-18-218-16-34.us-east-2.compute.amazonaws.com/sign_s3",{
+    fetch("//ec2-18-218-16-34.us-east-2.compute.amazonaws.com/sign_s3",{
       method: "post",
       headers: {
         'Accept': 'application/json',
